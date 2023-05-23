c69ccff65c077f1e49c11d6a9a7b4584dd4f59c4
blobby <blobby@yandex.ru>
2023-05-23 21:22:31 +0300

    add second adc

--- base-module/base-module.ino
+++ base-module/base-module.ino
@@ -18,17 +18,22 @@ void setup() {
 
   pulsometer_begin();
   adc_1_galvanic_begin();
+  adc_2_galvanic_begin();
   giroskop_axelerometer_begin();
 }
 
 void loop() {
   pulsometer_read(100);
   adc_1_galvanic_read();
+  adc_2_galvanic_read();
   giroskop_axelerometer_read();
   tenzo_line_read();
 
   Serial.print(adc_1_galvanic_value); Serial.print(" ");
   Serial.print(adc_1_galvanic_volt); Serial.print(" ");
+
+  Serial.print(adc_2_galvanic_value); Serial.print(" ");
+  Serial.print(adc_2_galvanic_volt); Serial.print(" ");
 
   Serial.print(tenzo_line); Serial.print(" ");
 
@@ -47,7 +52,8 @@ void loop() {
 }
 
 // ########################################################
-const int MPU_addr = 0x68; // адрес датчика
+// https://alexgyver.ru/arduino-mpu6050/
+#define MPU_addr 0x68 // адрес датчика
 
 void giroskop_axelerometer_begin ()
 {
@@ -104,14 +110,14 @@ int16_t tenzo_line_read ()
 }
 
 // #######################################################
+// https://arduino.ru/forum/apparatnye-voprosy/mini-obzor-atsp-mcp3421-s-vykhodom-i2c 
+
 #define adc_1_galvanic__ADDRESS	0x68
 
 void adc_1_galvanic_begin () {
   Wire.beginTransmission(adc_1_galvanic__ADDRESS);//i2c адрес MCP3421= B1101000
   Wire.write(B11000); // настройка АЦП: постоянное преобразование (1), 16бит(10), усиление=1(00). https://static.chipdip.ru/lib/011/DOC013011388.pdf str 11
   Wire.endTransmission();
-
-  // https://arduino.ru/forum/apparatnye-voprosy/mini-obzor-atsp-mcp3421-s-vykhodom-i2c 
 }
 
 void adc_1_galvanic_read () {
@@ -121,4 +127,30 @@ void adc_1_galvanic_read () {
   adc_1_galvanic_value= ((adc_1_galvanic_value<<8)| Wire.read()); //упаковка в одну переменную.
   adc_1_galvanic_value= ((adc_1_galvanic_value<<8)| Wire.read()); //упаковка в одну переменную.
   adc_1_galvanic_volt = adc_1_galvanic_value * 2.048 /131072 ; // LSB=15uV
+}
+
+// #######################################################
+// https://www.instructables.com/Arduino-and-the-TI-ADS1110-16-bit-ADC/
+
+#define adc_2_galvanic__ADDRESS	0x48
+
+void adc_2_galvanic_begin () {
+  return;
+}
+
+void adc_2_galvanic_read () {
+  int8_t highbyte, lowbyte, configRegister;
+
+  Wire.requestFrom(adc_2_galvanic__ADDRESS, 3);
+  while (Wire.available() ) // ensure all the data comes in
+  {
+    highbyte = Wire.read(); // high byte * B11111111
+    lowbyte = Wire.read(); // low byte
+    configRegister = Wire.read();
+  }
+
+  adc_2_galvanic_value = highbyte * 256;
+  adc_2_galvanic_value = adc_2_galvanic_value + lowbyte;
+  adc_2_galvanic_volt = adc_2_galvanic_value * 2.048 ;
+  adc_2_galvanic_volt = adc_2_galvanic_volt / 32768.0;
 }
